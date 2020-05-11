aa58307c3c8349fd9c0fff4aeaa1926a95acb241
Rich Wicentowski <richardw@cs.swarthmore.edu>
2020-05-11 13:39:09 -0400

    removed PA from the title

--- web/index.php
+++ web/index.php
@@ -1,10 +1,10 @@
 <html>
   <head>
-    <title>Covid-19 Graphs for PA</title>   
+    <title>Covid-19 Graphs</title>   
     <link rel="stylesheet" type="text/css" href="mystyle.css">
   </head>
   <body>
-    <h1>Covid-19 Graphs for PA</h1>
+    <h1>Covid-19 Graphs</h1>
     <h3>Data Last Updated:  <?php
 echo date('l F d, Y \a\t H:i:s', filemtime("/home/richardw/Dropbox/covid/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports"));
 ?> 
@@ -27,7 +27,7 @@ echo date('l F d, Y \a\t H:i:s', filemtime("/home/richardw/Dropbox/covid/COVID-1
 
     <h4>
       <p>
-	Data from the <a href="https://github.com/CSSEGISandData/COVID-19">JHU CSSE COVID-19 Dataset</a> and <a href="https://www.census.gov/data/datasets/time-series/demo/popest/2010s-counties-total.html">the US Census Bureau</a>.<BR />
+	Data from the <a href="https://github.com/CSSEGISandData/COVID-19">JHU CSSE COVID-19 Dataset</a> and the <a href="https://www.census.gov/data/datasets/time-series/demo/popest/2010s-counties-total.html">US Census Bureau</a>.<BR />
       Source code and data available from <a href="https://github.com/richwiss/covid">github</a>. Pull requests and comments welcome.<BR /> 
 	Created by <a href="https://twitter.com/richwiss/">@richwiss</a>
       </p>
