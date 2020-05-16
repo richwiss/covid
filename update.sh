a52b09a6fb0a6ea95d766cc6e9e196b5f3705959
Rich Wicentowski <richardw@cs.swarthmore.edu>
2020-05-16 00:54:35 -0400

    pause to let dropbox catch up in case this is run remotely

--- update.sh
+++ update.sh
@@ -9,12 +9,14 @@ cd -
 # create new graphs
 jupyter nbconvert --to notebook --execute plots.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.kernel_name=python3
 
-
 # create tables
 for state in $(ls states); do
     echo "Generating table for $state";
     python3 make_table.py -s $state
 done
+
+# give dropbox a second 
+sleep 1
 
 # be sure files are readable on the server
 ssh ginger chmod -R a+rX /dropbox/richardw/Dropbox/covid/web/ /dropbox/richardw/Dropbox/covid/states/
