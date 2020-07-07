8375107b232922d27ff7b8c3fce12f72b627c58a
Rich Wicentowski <richardw@cs.swarthmore.edu>
2020-07-07 00:00:03 -0400

    tweaks

--- update.sh
+++ update.sh
@@ -8,18 +8,17 @@ if [ -z ${VIRTUAL_ENV+x} ]; then
 fi
 
 # update data from JHU
-cd COVID-19
-git pull
-cd -
-
-# create new graphs
-STATEPLOT="ALL" ipython3 plots.ipynb
+cd COVID-19 ; git pull ; cd ..
 
 if [ -z ${COVIDDIR+x} ]; then
-    export STATEDIR="~/Desktop/states"
-else
-    export STATEDIR="${COVIDDIR}/states"
+    export COVIDDIR="~/covid"
 fi
+export STATEDIR="${COVIDDIR}/states"
+
+# create new graphs
+ipython3 plots2.ipynb -- --states ALL --graph_directory $COVIDDIR
+#ipython3 plots2.ipynb -- --states Maine --graph_directory $COVIDDIR --ignore_timestamp
+#STATEPLOT="ALL" ipython3 plots.ipynb
 
 # update tables
 for state in Pennsylvania Florida Georgia New_Jersey New_York California North_Carolina Alabama Alaska Arizona Arkansas Colorado Connecticut Delaware District_of_Columbia Guam Hawaii Idaho Illinois Indiana Iowa Kansas Kentucky Louisiana Maine Maryland Massachusetts Michigan Minnesota Mississippi Missouri Montana Nebraska Nevada New_Hampshire New_Mexico North_Dakota Northern_Mariana_Islands Ohio Oklahoma Oregon  Rhode_Island South_Carolina South_Dakota Tennessee Texas Utah Vermont Virginia Virgin_Islands Washington West_Virginia Wisconsin Wyoming; do
@@ -32,7 +31,6 @@ for state in Pennsylvania Florida Georgia New_Jersey New_York California North_C
         chmod a+rX ${STATEDIR}/${state}/graphs.php 
     fi
 done
-
 
 if [ "${STATEDIR}/index.php" -ot "COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv" ]; then
     python3 make_table_states.py
