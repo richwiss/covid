18e161a228056f8c9b449bb61f2579510b9a0cc7
Rich Wicentowski <richardw@cs.swarthmore.edu>
2021-01-03 23:15:53 -0500

    removed unneeded imports

--- viz/bin/vaccines.py
+++ viz/bin/vaccines.py
@@ -1,13 +1,6 @@
-# Environment used: dash1_8_0_env
-import pandas as pd     #(version 1.0.0)
-import plotly           #(version 4.5.0)
+import pandas as pd
+import plotly
 import plotly.express as px
-
-import dash             #(version 1.8.0)
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
-from dash.exceptions import PreventUpdate
 
 df = pd.read_csv('../../data/owid-covid-19-data/public/data/vaccinations/vaccinations.csv')
 cc = pd.read_csv('../data/country_codes.csv')
@@ -17,21 +10,23 @@ df['iso'] = df.location.apply(lambda x: codes[x])
 df['dt'] = pd.to_datetime(df['date'])
 df['date'] = df['dt'].dt.strftime("%Y-%m-%d")
 df = df.loc[df.groupby('location').dt.idxmax()]
-df.rename(columns = {"total_vaccinations_per_hundred": "vaccines per 100"},  inplace = True) 
+df.rename(columns = {"total_vaccinations_per_hundred": "pct vaccinated"},  inplace = True) 
 
 #---------------------------------------------------------------
 
 def generate_figure():
     fig = px.choropleth(df, locations="iso",
-                        color="vaccines per 100",
+                        color="pct vaccinated",
                         hover_name="location",
                         hover_data=['date'],
                         projection='natural earth',
-                        title='Vaccines administered per 100 people',
-                        color_continuous_scale=px.colors.sequential.Plasma)
+                        title='Percentage of people vaccinated',
+                        range_color=(0,5),
+                        color_continuous_scale=px.colors.sequential.Turbo)
 
     fig.update_layout(title=dict(font=dict(size=28),x=0.5,xanchor='center'),
                         margin=dict(l=60, r=60, t=50, b=50))
+    #fig.update_layout(coloraxis_showscale=False)
 
     return fig
 
